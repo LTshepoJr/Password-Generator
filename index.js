ba8a4aca6c4ddfbe96b7c55b1cc4df8c9ee78b82
Lethabo Kgoele <lethabotshepojr@gmail.com>
2024-03-26 18:51:28 +0200

    HTML & JavaScript Done

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
- "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
- "~","`","!","@","#","$","%","^","&","*","(",")","_","-",
- "+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-// make an object array to make a option to choose what the user wants and make a new array to push it inside
+"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = [ "~","`","!","@","#","$","%","^","&","*","(",")","_","-",
+"+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+let selectedOption1 = document.getElementById("numberCheck");
+let selectedOption2 = document.getElementById("symbolsCheck");
+
+selectedOption1.addEventListener("change", checkboxChanged);
+selectedOption2.addEventListener("change", checkboxChanged);
+
+
+function checkboxChanged() {
+    if (selectedOption1.checked && selectedOption2.checked) {
+        for (let i = 0;i < numbers.length;i++) {
+            characters.push(numbers[i])
+        }
+
+        for (let i = 0;i < symbols.length;i++) {
+            characters.push(symbols[i])
+        }
+    } else if (selectedOption1.checked) {
+        for (let i = 0;i < numbers.length;i++) {
+            characters.push(numbers[i])
+        }
+    } else if (selectedOption2.checked) {
+        for (let i = 0;i < symbols.length;i++) {
+            characters.push(symbols[i])
+        }
+    } else {
+        document.getElementById("result").innerHTML = `<p>Please select at least one option.</p>`;
+    }
+}
+
+
 const button = document.getElementById("btn")
 const box1 = document.getElementById("firstBox")
 const box2 = document.getElementById("secondBox")
@@ -14,15 +44,23 @@ const submitValue = document.getElementById("myForm")
 
 
 button.addEventListener("click", function(){
-    numCharacters()
+    if (selectedOption1.checked || selectedOption2.checked) {
+        checkboxChanged()
+        numCharacters()
+    }
 })
 
-function randomCharaters(array, count) {
-    let characters2 = []
-    for (let i = 0;i < count;i++) {
-        let newarr = Math.floor(Math.random()*characters.length)
-        characters2.push(array[newarr])
-    } return characters2
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function randomCharaters() {
+    let randomPassword = ""
+    for (let i = 0; i < number.value; i++) {
+        randomPassword += getRandomCharacter()         
+    }
+    return randomPassword
 }
 
 submitValue.addEventListener("click", function(event) {
@@ -31,36 +69,36 @@ submitValue.addEventListener("click", function(event) {
 
 function numCharacters() {
     if (number === 8) {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     } 
     else if (number === 9) {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     }
     else if (number === 10) {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     }
     else if (number === 11) {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     }
     else if (number === 12) {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     }
     else if (number === 13) {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     }
     else if (number === 14) {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     }
     else {
-        box1.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
-        box2.innerHTML = `<p>${randomCharaters(characters, number.value)}</p>`
+        box1.innerHTML = `<p>${randomCharaters()}</p>`
+        box2.innerHTML = `<p>${randomCharaters()}</p>`
     }
 }
 
@@ -85,3 +123,8 @@ box2.addEventListener("click", function() {
     alert("Text copied to clipboard: " + textCopy)
 })
 
+var refreshButton = document.getElementById("refreshButton");
+
+refreshButton.addEventListener("click", function() {
+    location.reload();
+});
